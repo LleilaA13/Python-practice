405aa9cabed8118ca29ce71cf66b5740f98d8734
LleilaA13 <zanonileila3@gmail.com>
2023-09-05 13:05:52 +0200

    dictionnaries and sets, using .union and .intersection

--- exercise15/program.py
+++ exercise15/program.py
@@ -16,6 +16,34 @@ allora la funzione  restituisce il dizionario
 
 
 def es18(d1, d2):
-    pass
+    diz = {}
+    set1 = set(d1)
+    set2 = set(d2)
+    # print(set1, set2)
+    set3 = set1.intersection(set2)
+
+    lista = list(set3)
+    set_union = set()
+    set_intersection = set()
+    # print(lista)
+    for k in lista:
+        diz[k] = (set_intersection, set_union)
+    for k in d1.keys():
+        if k in d2.keys():
+            set_union = d1[k] | d2[k]
+            set_intersection = d1[k] & d2[k]
+            diz[k] = (set_intersection,set_union)
+    return diz
+
+    #intersection_set = set1.intersection(set2)
+    #print(union_set,intersection_set)
+
+
+    # set_valori1 = set(lista_valori1)
+    # set_valori2 = set(lista_valori2)
+    # diz = (set_valori1.union(set_valori2), set_valori1.intersection(set_valori2))
+
+
 if __name__ == '__main__':
     print(es18({1: {1, 2, 3}, 2: {1, 2, 3}, 5: {1}}, {1: {3, 4, 5}, 3: {1, 2, 3}, 5: {3}, 8: {6}}))
+    'Daje Leylaa che sei brava bravaa!!'
