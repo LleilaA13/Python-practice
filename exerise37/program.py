8bc8903478bea36bddc06b2bbc887bfdfef394cd
LleilaA13 <zanonileila3@gmail.com>
2024-02-23 12:19:24 +0100

    practice with dictionaries, lambda and sorting

--- exerise37/program.py
+++ exerise37/program.py
@@ -4,38 +4,37 @@
     sum of the first i integers of S.
     For example: the sequence derived from 
     S= 2, -3, -4, 4, 4, -5, 3, 1.-1 is
-      2, -1, -5, -1, 3, -2, 1, 2, 1.
+    2, -1, -5, -1, 3, -2, 1, 2, 1.
     
     Design and implement a function ex41(fname) such that:..................
     - it takes as arguments the path of a text file 'fname', where it
-      is stored a sequence S of integers, separated by a comma and
-      some white spaces
+        is stored a sequence S of integers, separated by a comma and
+        some white spaces
     - it returns the number that appears most frequently in the
-      sequence derived from S. If there are several numbers with the
-      highest number of repetitions, the maximum one is returned.
+        sequence derived from S. If there are several numbers with the
+        highest number of repetitions, the maximum one is returned.
     Example: if the 'fname' file contains the sequence S= 2, -3, -4,
     4, -5, 3, 1, -1 the function returns the value 2.
     '''
+
+
 def ex41(fname):
-  with open(fname, encoding='utf8') as f:
-    s = [int(x) for x in f.read().split(',')]
-  derived = []
-  sum = 0
-  for x in s:
-    sum += x
-    derived.append(sum)
-  freq = {}
-  for x in derived:
-    if x in freq:
-      freq[x] += 1
-    else:
-      freq[x] = 1
-  pairs = sorted(freq, key = lambda x : ())
-  print(pairs)
+    with open(fname, encoding = 'utf8') as f:
+        seq = [int(x) for x in f.read().split(',')]
+    sum = 0
+    lista = []
+    for i in range(len(seq)):
+        sum += seq[i]
+        lista.append(sum)
+    freq = {}
+    for x in lista:
+        if x in freq:
+            freq[x] += 1
+        if x not in freq:
+            freq[x] = 1
+    pairs = max(freq.keys(), key = lambda k:(freq[k], k))
+    return pairs
 
 
 if __name__ == '__main__':
-  ex41('fsequenza1.txt')
-
-
-
+    ex41('fsequenza1.txt')
